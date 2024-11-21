1035168e576dd24f1b67f18ef6d03e7be19010fa
PeggyBevan <ucbtbev@ucl.ac.uk>
2024-11-21 12:17:14 +0000

    remove data files due to data protection issues

--- Data/PredictsData/roquefort/R/construct_call.R
+++
@@ -1,7 +0,0 @@
-construct_call<-function(responseVar,fixedStruct,randomStruct){
-  return(paste(responseVar,"~",fixedStruct,"+",randomStruct,sep=""))
-}
-
-construct_call_admb<-function(responseVar,fixedStruct){
-  return(paste(responseVar,"~",fixedStruct,sep=""))
-}
